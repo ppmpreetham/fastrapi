0dc90049872c681605305aec535f7d0396cb0cc1
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-09-24 23:29:33 +0530

    add data structure and pymodule

--- fasterapi/src/lib.rs
+++ fasterapi/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
+use pyo3::prelude::*;
+use once_cell::sync::Lazy;
+use std::collections::HashMap;
+use std::sync::Mutex;
+
+use hyper::service::service_fn;
+
+// Lazy initialization of Mutex of Hashmap : /about -> function
+static ROUTES: Lazy<Mutex<HashMap<String, Py<PyAny>>>> = Lazy::new(|| Mutex::new(HashMap::new()));
+
+#[pyfunction]
+fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[pymodule]
+fn fasterapi(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(add, m)?)?;
+    Ok(())
 }
