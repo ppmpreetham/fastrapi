4db9c528d514701f56a2ed33c4444baa6bcb8d38
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-12-15 02:01:24 +0530

    add: generalize the rotation part

--- src/components/ContentRotate.tsx
+++ src/components/ContentRotate.tsx
@@ -14,6 +14,7 @@ export const Rotate1 = ({ place }: { place: boolean }) => {
     <group>
       <RotHeaderText place={place} text="33x Faster" offset={[0, 0.5, 0]} />
       <RotHeaderText place={place} text="than FastAPI" size={0.5} offset={[0, -0.5, 0]} />
+      {/* feat: add many more here (performance metrics and other stuff) */}
     </group>
   );
 };
@@ -21,8 +22,18 @@ export const Rotate1 = ({ place }: { place: boolean }) => {
 export const Rotate2 = ({ place }: { place: boolean }) => {
   return (
     <group>
-      <RotSubText place={place} text="Type Safe support for" size={0.5} offset={[0, 1, 0]} />
+      <RotSubText place={place} text="Features Include" size={0.5} offset={[0, 1, 0]} />
       <RotHeaderText place={place} text="PyDantic" />
+      {/* feat: add many more here (images and other stuff) */}
+    </group>
+  );
+};
+export const Rotate3 = ({ place }: { place: boolean }) => {
+  return (
+    <group>
+      <RotSubText place={place} text="Just change" size={0.5} offset={[0, 1, 0]} />
+      <RotHeaderText place={place} text="ONE CHARACTER" />
+      {/* feat: show change from FastAPI to FastRAPI */}
     </group>
   );
 };
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,35 +1,46 @@
-import { FastrapiModel } from "../models/fastrapimodel";
-import { Environment, PerspectiveCamera } from "@react-three/drei";
-import { OrbitControls, ScrollControls, useScroll } from "@react-three/drei";
+import { useRef, useState } from "react";
+import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
+import {
+  Environment,
+  PerspectiveCamera,
+  OrbitControls,
+  ScrollControls,
+  useScroll,
+} from "@react-three/drei";
+
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+
+import { FastrapiModel } from "../models/fastrapimodel";
 import { CrystalField } from "../models/crystals/MultiChristal";
-import * as THREE from "three";
-import { useRef, useState } from "react";
-import { useFrame } from "@react-three/fiber";
-import { Rotate0, Rotate1, Rotate2 } from "./ContentRotate";
+
+import { Rotate0, Rotate1, Rotate2, Rotate3 } from "./ContentRotate";
+const rotateComponents = [Rotate0, Rotate1, Rotate2, Rotate3];
+const length = rotateComponents.length;
 
 const PI = Math.PI;
 
 function CameraScroller({
   cameraRef,
   tl,
+  setCount,
+  count,
 }: {
   cameraRef: React.RefObject<THREE.PerspectiveCamera>;
   tl: React.RefObject<gsap.core.Timeline>;
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+  count: number;
 }) {
   const scroll = useScroll();
   const radius = 10;
-  const [count, updateCount] = useState<number>(1);
 
   useFrame(() => {
-    const angle = scroll.offset * 2 * PI;
-
+    const angle = scroll.offset * (length - 1) * PI;
     const newCount = Math.floor((angle - PI / 2) / PI) + 1;
     if (newCount !== count) {
-      updateCount(newCount);
+      setCount(newCount);
     }
-    console.log(newCount);
     const x = Math.sin(angle) * radius;
     const z = Math.cos(angle) * radius;
     if (cameraRef.current) {
@@ -47,6 +58,7 @@ const Experience = () => {
   const groupRef = useRef<THREE.Mesh>(null!);
   const tl = useRef<gsap.core.Timeline>(null!);
   const cameraRef = useRef<THREE.PerspectiveCamera>(null!);
+  const [count, setCount] = useState<number>(1);
 
   useGSAP(() => {
     tl.current = gsap.timeline();
@@ -56,14 +68,16 @@ const Experience = () => {
     <>
       <OrbitControls enableZoom={false} enablePan={false} />
       <Environment preset="dawn" />
-      <ScrollControls pages={3} damping={0.25} infinite={false}>
+      <ScrollControls pages={length} damping={0.25} infinite={false}>
         <PerspectiveCamera makeDefault position={[0, 0, 10]} ref={cameraRef} />
-        <CameraScroller cameraRef={cameraRef} tl={tl} />
+        <CameraScroller cameraRef={cameraRef} tl={tl} setCount={setCount} count={count} />
         <group ref={groupRef}>
           <CrystalField />
           <FastrapiModel ref={logoRef} />
-          <Rotate0 place={false} />
-          <Rotate1 place={true} />
+          {(() => {
+            const Rotate = rotateComponents[count];
+            return Rotate ? <Rotate place={count % 2 === 1} /> : null;
+          })()}
         </group>
       </ScrollControls>
     </>
