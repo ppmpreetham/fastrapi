cd9b03b6bdce770e2f85d1176b5be0e63487dc08
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-03-05 01:07:40 +0530

    remove: docs

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,6 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/docs" element={<Docs />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
