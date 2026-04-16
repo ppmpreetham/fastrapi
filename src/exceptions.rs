a6ae23a86f29df3d5d47617c0cdc4be598d5f762
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-04-16 15:21:05 +0530

    perf(exceptions): we use bounds now

--- src/exceptions.rs
+++ src/exceptions.rs
@@ -5,7 +5,7 @@ use pyo3::prelude::*;
 use pyo3::types::{PyDict, PyTuple};
 use serde_json::json;
 
-// --- Base Errors ---
+// Base Errors
 
 #[pyclass(extends = PyRuntimeError, name = "FastrAPIError")]
 pub struct PyFastrAPIError;
@@ -27,20 +27,28 @@ pub struct PyValidationException {
 #[pymethods]
 impl PyValidationException {
     #[new]
-    fn new(_errors: Py<PyAny>) -> Self {
-        Self { _errors }
+    fn new(errors: Bound<'_, PyAny>) -> Self {
+        Self {
+            _errors: errors.into(),
+        }
     }
 
     #[pyo3(signature = (*_args, **_kwargs))]
     fn __init__(&self, _args: &Bound<'_, PyTuple>, _kwargs: Option<&Bound<'_, PyDict>>) {}
 
-    fn errors(&self) -> Py<PyAny> {
-        self._errors.clone()
+    fn errors<'py>(&self, py: Python<'py>) -> Bound<'py, PyAny> {
+        self._errors.bind(py).clone()
     }
 
     fn __str__(&self, py: Python<'_>) -> String {
         let errors = self._errors.bind(py);
-        let len = errors.len().unwrap_or(0);
+        let len = if let Ok(list) = errors.cast::<pyo3::types::PyList>() {
+            list.len()
+        } else if let Ok(dict) = errors.cast::<pyo3::types::PyDict>() {
+            dict.len()
+        } else {
+            0
+        };
         format!(
             "{} validation error{} occurred",
             len,
@@ -49,7 +57,7 @@ impl PyValidationException {
     }
 }
 
-// --- Request/Response Validation Errors ---
+// Request/Response Validation Errors
 
 #[pyclass(extends = PyValidationException, name = "RequestValidationError")]
 pub struct PyRequestValidationError {
@@ -61,9 +69,16 @@ pub struct PyRequestValidationError {
 impl PyRequestValidationError {
     #[new]
     #[pyo3(signature = (errors, *, body=None))]
-    fn new(errors: Py<PyAny>, body: Option<Py<PyAny>>) -> (Self, PyValidationException) {
-        let body = body.unwrap_or_else(|| Python::attach(|py| py.None()));
-        (Self { body }, PyValidationException::new(errors))
+    fn new(
+        py: Python<'_>,
+        errors: Bound<'_, PyAny>,
+        body: Option<Bound<'_, PyAny>>,
+    ) -> (Self, PyValidationException) {
+        let body_py = body.map(|b| b.into()).unwrap_or_else(|| py.None());
+        (
+            Self { body: body_py },
+            PyValidationException::new(errors.into()),
+        )
     }
 
     #[pyo3(signature = (*_args, **_kwargs))]
@@ -80,16 +95,23 @@ pub struct PyResponseValidationError {
 impl PyResponseValidationError {
     #[new]
     #[pyo3(signature = (errors, *, body=None))]
-    fn new(errors: Py<PyAny>, body: Option<Py<PyAny>>) -> (Self, PyValidationException) {
-        let body = body.unwrap_or_else(|| Python::attach(|py| py.None()));
-        (Self { body }, PyValidationException::new(errors))
+    fn new(
+        py: Python<'_>,
+        errors: Bound<'_, PyAny>,
+        body: Option<Bound<'_, PyAny>>,
+    ) -> (Self, PyValidationException) {
+        let body_py = body.map(|b| b.into()).unwrap_or_else(|| py.None());
+        (
+            Self { body: body_py },
+            PyValidationException::new(errors.into()),
+        )
     }
 
     #[pyo3(signature = (*_args, **_kwargs))]
     fn __init__(&self, _args: &Bound<'_, PyTuple>, _kwargs: Option<&Bound<'_, PyDict>>) {}
 }
 
-// --- HTTP Exceptions ---
+// HTTP Exceptions
 
 #[pyclass(extends = PyException, name = "HTTPException", skip_from_py_object)]
 #[derive(Clone)]
@@ -109,14 +131,13 @@ impl PyHTTPException {
     fn new(
         py: Python<'_>,
         status_code: u16,
-        detail: Option<Py<PyAny>>,
-        headers: Option<Py<PyDict>>,
+        detail: Option<Bound<'_, PyAny>>,
+        headers: Option<Bound<'_, PyDict>>,
     ) -> Self {
-        let detail = detail.unwrap_or_else(|| py.None());
         Self {
             status_code,
-            detail,
-            headers,
+            detail: detail.map(|d| d.into()).unwrap_or_else(|| py.None()),
+            headers: headers.map(|h| h.into()),
         }
     }
 
@@ -124,13 +145,8 @@ impl PyHTTPException {
     fn __init__(&self, _args: &Bound<'_, PyTuple>, _kwargs: Option<&Bound<'_, PyDict>>) {}
 
     fn __str__(&self, py: Python<'_>) -> String {
-        let s = self
-            .detail
-            .bind(py)
-            .str()
-            .map(|s| s.to_string_lossy().to_string())
-            .unwrap_or_default();
-        format!("{}: {}", self.status_code, s)
+        let detail = self.detail.bind(py);
+        format!("{}: {}", self.status_code, detail)
     }
 
     fn __repr__(&self, py: Python<'_>) -> String {
@@ -150,13 +166,13 @@ impl PyHTTPException {
 impl PyHTTPException {
     pub fn to_response(&self, py: Python<'_>) -> Response {
         let status =
-            StatusCode::from_u16(self.status_code).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
+            StatusCode::try_from(self.status_code).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
         let detail_json = crate::utils::py_any_to_json(py, self.detail.bind(py));
         (status, Json(json!({ "detail": detail_json }))).into_response()
     }
 }
 
-// --- WebSocket Exceptions ---
+// WebSocket Exceptions
 
 #[pyclass(extends = PyException, name = "WebSocketException", skip_from_py_object)]
 #[derive(Clone)]
